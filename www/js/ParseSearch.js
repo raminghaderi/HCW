6c8828f79afeb9d2af684e2fcfec4064f32e2808
Raminho <raminho_luiz@yahoo.com>
2015-11-14 21:14:58 +0400

    Very mast

---
+++ www/js/ParseSearch.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Raminho on 11/14/2015.
+ */
+function mast () {
+
+    var ID = Parse.Object.extend("IDs");
+    var query = new Parse.Query(ID);
+
+    var search = document.getElementById("search").value;
+
+    query.equalTo("ids", search);
+    query.find({
+        success: function (results) {
+
+            if(results.length<=0)
+            {
+                alert("Invalid Patient ID ");
+            }
+            else {
+
+                window.location.href="../patientInfo.html";
+            }
+        },
+        error: function (error) {
+            alert("Invalid Patient ID " + error.code + " " + error.message);
+        }
+    });
+}
---
+++ www/js/parseInit.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Raminho on 11/14/2015.
+ */
+Parse.initialize("pRuiPyhzLD8qOfQxnKOyCyQAkrdIQyZKcQt3184m", "IF0VqIohNLvo9mVZ10gX5wvaR8wtXcnoQhfeqPVs");
